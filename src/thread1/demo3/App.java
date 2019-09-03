252180e9a2358a0b50b33e64676d3a8a7d8f23b6
Sebastian Cuatepotzo <elcuato@localhost.localdomain>
2019-09-03 17:52:39 -0500

    Class 1

---
+++ src/thread1/demo3/App.java
@@ -0,0 +1,28 @@
+package thread1.demo3;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Thread t1 = new Thread(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				for (int i = 0; i < 10; i++) {
+					System.out.println("Hello " + i);
+					
+					try {
+						Thread.sleep(100);
+					} catch (InterruptedException e) {
+					}
+				} //End For Loop
+				
+			}//End redefined RUN method
+		}); //End declaration new Thread
+		
+		t1.start();
+		
+	}
+
+}
